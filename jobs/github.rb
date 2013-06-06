353644d9f186db387e8d92c417b74830a80eab94
Michael Chang <thenewme91@gmail.com>
2013-06-06 19:37:46 -0400

    last commit job

--- jobs/github.rb
+++ jobs/github.rb
@@ -1,17 +1,19 @@
-require 'net/http'
-require 'json'
-
-search_term = URI::encode('#todayilearned')
+require 'octokit'
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
-  http = Net::HTTP.new('search.twitter.com')
-  response = http.request(Net::HTTP::Get.new("/search.json?q=#{search_term}"))
-  tweets = JSON.parse(response.body)["results"]
-  if tweets
-    tweets.map! do |tweet| 
-      { name: tweet['from_user'], body: tweet['text'], avatar: tweet['profile_image_url_https'] }
-    end
-  
-    send_event('twitter_mentions', comments: tweets)
+  repos = ["zmanji/ecryptfs", "zmanji/ecryptfs_userspace",
+  "cbhl/fydp_build", "cbhl/fydp_dashing"]
+
+  commits = repos.map do |repo|
+      {
+          name: repo,
+          latest_commit: Octokit.commits(repo, nil, options={:per_page => 1})[0],
+      }
   end
+
+  result = commits.map do |repo|
+      { label: repo[:name], value: repo[:latest_commit].sha[0..7] }
+  end
+
+  send_event('last-commit', {items: result})
 end
